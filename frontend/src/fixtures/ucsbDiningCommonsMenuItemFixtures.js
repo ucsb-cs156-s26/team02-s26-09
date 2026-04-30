7ae7d215d65b075124d89ed447aecfecedb554a9
Parth Mohnot <pmohnot@ucsb.edu>
2026-04-30 12:01:33 -0700

    added fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+  oneDate: {
+    id: 1,
+    diningCommonsCode: "Portola",
+    name: "Mac and Cheese",
+    station: "Pizza Hub",
+  },
+  threeDates: [
+    {
+      id: 1,
+      diningCommonsCode: "Ortega",
+      name: "Chicken Tenders",
+      station: "Main Station",
+    },
+    {
+      id: 2,
+      diningCommonsCode: "DLG",
+      name: "Chicken Tenders",
+      station: "Chefs Choice",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "Carrillo",
+      name: "Green Beans",
+      station: "Sides Station",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
