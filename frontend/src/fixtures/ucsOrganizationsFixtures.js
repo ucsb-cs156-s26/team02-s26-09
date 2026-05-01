c0d47be458a6e5298093982acf049c57b4341b97
Ryan Longacre <ryanlongacre@ucsb.edu>
2026-05-01 16:54:08 -0700

    rl - fixed frontend formatting

--- frontend/src/fixtures/ucsOrganizationsFixtures.js
+++ frontend/src/fixtures/ucsOrganizationsFixtures.js
@@ -1,29 +1,29 @@
 const ucsbOrganizationsFixtures = {
   oneOrganization: {
-      "orgCode": "ZPR",
-      "orgTranslationShort": "ZETA PHI RHO",
-      "orgTranslation": "ZETA PHI RHO",
-      "inactive": false
+    orgCode: "ZPR",
+    orgTranslationShort: "ZETA PHI RHO",
+    orgTranslation: "ZETA PHI RHO",
+    inactive: false,
   },
   threeOrganizations: [
     {
-      "orgCode": "ZPR",
-      "orgTranslationShort": "ZETA PHI RHO",
-      "orgTranslation": "ZETA PHI RHO",
-      "inactive": false
+      orgCode: "ZPR",
+      orgTranslationShort: "ZETA PHI RHO",
+      orgTranslation: "ZETA PHI RHO",
+      inactive: false,
     },
     {
-      "orgCode": "SKY",
-      "orgTranslationShort": "SKYDIVING CLUB",
-      "orgTranslation": "SKYDIVING CLUB AT UCSB",
-      "inactive": false
+      orgCode: "SKY",
+      orgTranslationShort: "SKYDIVING CLUB",
+      orgTranslation: "SKYDIVING CLUB AT UCSB",
+      inactive: false,
     },
     {
-      "orgCode": "OSLI",
-      "orgTranslationShort": "STUDENT LIFE",
-      "orgTranslation": "OFFICE OF STUDENT LIFE",
-      "inactive": false
-    }
+      orgCode: "OSLI",
+      orgTranslationShort: "STUDENT LIFE",
+      orgTranslation: "OFFICE OF STUDENT LIFE",
+      inactive: false,
+    },
   ],
 };
 
