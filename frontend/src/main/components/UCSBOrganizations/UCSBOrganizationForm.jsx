9ae946abf7e3765bbb942b4c967b392b5843a7b0
Ryan Longacre <ryanlongacre@ucsb.edu>
2026-05-01 18:13:40 -0700

    rl - WIP fixed formating

--- frontend/src/main/components/UCSBOrganizations/UCSBOrganizationForm.jsx
+++ frontend/src/main/components/UCSBOrganizations/UCSBOrganizationForm.jsx
@@ -39,7 +39,9 @@ function UCSBOrganizationForm({
       </Form.Group>
 
       <Form.Group className="mb-3">
-        <Form.Label htmlFor="orgTranslationShort">Org Translation Short</Form.Label>
+        <Form.Label htmlFor="orgTranslationShort">
+          Org Translation Short
+        </Form.Label>
         <Form.Control
           data-testid={testIdPrefix + "-orgTranslationShort"}
           id="orgTranslationShort"
