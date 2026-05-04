888632c376876767ac2589cc11e955b39c6d410e
Ryan Longacre <ryanlongacre@ucsb.edu>
2026-05-03 18:32:10 -0700

    rl - UCSBOrganizations edit page and tests

--- frontend/src/main/pages/UCSBOrganizations/UCSBOrganizationsEditPage.jsx
+++ frontend/src/main/pages/UCSBOrganizations/UCSBOrganizationsEditPage.jsx
@@ -1,11 +1,78 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router";
+import UCSBOrganizationForm from "main/components/UCSBOrganizations/UCSBOrganizationForm";
+import { Navigate } from "react-router";
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function UCSBOrganizationsEditPage() {
-  // Stryker disable all : placeholder for future implementation
+export default function UCSBOrganizationsEditPage({ storybook = false }) {
+  let { orgCode } = useParams();
+
+  const {
+    data: ucsborganization,
+    _error,
+    _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    [`/api/ucsborganizations?orgCode=${orgCode}`],
+    {
+      // Stryker disable next-line all : GET is the default, so mutating this to "" doesn't introduce a bug
+      method: "GET",
+      url: `/api/ucsborganizations`,
+      params: {
+        orgCode,
+      },
+    },
+  );
+
+  const objectToAxiosPutParams = (ucsborganization) => ({
+    url: "/api/ucsborganizations",
+    method: "PUT",
+    params: {
+      orgCode: ucsborganization.orgCode,
+    },
+    data: {
+      orgCode: ucsborganization.orgCode,
+      orgTranslationShort: ucsborganization.orgTranslationShort,
+      orgTranslation: ucsborganization.orgTranslation,
+      inactive: ucsborganization.inactive,
+    },
+  });
+
+  const onSuccess = (ucsborganization) => {
+    toast(
+      `UCSBOrganization Updated - orgCode: ${ucsborganization.orgCode} orgTranslation: ${ucsborganization.orgTranslation}`,
+    );
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/ucsborganizations?orgCode=${orgCode}`],
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/ucsborganizations" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Edit page not yet implemented</h1>
+        <h1>Edit UCSBOrganization</h1>
+        {ucsborganization && (
+          <UCSBOrganizationForm
+            submitAction={onSubmit}
+            buttonLabel={"Update"}
+            initialContents={ucsborganization}
+          />
+        )}
       </div>
     </BasicLayout>
   );
